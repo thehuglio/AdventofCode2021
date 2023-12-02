22ae4cdacdfd9f5d9ec37e117c493ba6ab2a5245
thehuglio <122838549+thehuglio@users.noreply.github.com>
2023-12-02 18:25:39 +0100

    Update Day 01.java

--- Day 01.java
+++ Day 01.java
@@ -21,3 +21,27 @@ public class Main {
         System.out.println(tot);
     }
 }
+//part 2
+package org.thehuglio;
+
+import java.io.File;
+import java.util.List;
+
+public class Main {
+
+    private static final List<String> data = new Reader(new File("data.txt")).data;
+
+
+    public static void main(String[] args) {
+        int depth = 10000000;
+        int tot = 0;
+        for (int i = 0; i < data.size() - 2; i++) {
+            int number = Integer.parseInt(data.get(i)) + Integer.parseInt(data.get(i+1)) + Integer.parseInt(data.get(i+2));
+            if (number > depth) {
+                tot++;
+            }
+            depth = number;
+        }
+        System.out.println(tot);
+    }
+}
